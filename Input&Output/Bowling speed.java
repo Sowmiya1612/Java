75a3f1fa51cc3df84283f25e6168aa70067e4e0b
Sowmiya.M <136424983+Sowmiya1612@users.noreply.github.com>
2023-07-01 07:37:21 +0530

    Create Bowling speed.java

---
+++ Input&Output/Bowling speed.java
@@ -0,0 +1,26 @@
+/*You are working as the bowling assist during the practice session before match. All the bowlers are asking the speed of the ball. So, you need to get the ball speed and print the ball speed with only 2 decimal value.
+
+Input Format:
+Input value consists of double value
+
+Output Format:
+Output value consists of double value with 2 decimal value.
+
+Sample Input:
+141.236
+
+Sample Output:
+141.24*/
+
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        //get the input value as double value from the user
+        double a;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextDouble();
+        System.out.println(a);
+    }
+}
